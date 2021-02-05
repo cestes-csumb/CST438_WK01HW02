c992fbcd0363410ebcd53dd852160acc9968eae3
Chris Estes <chestes@csumb.edu>
2021-02-05 14:54:21 -0800

    API code in place. No logins or userdatabase yet. All posts are displayed currently.

---
+++ app/src/main/java/com/example/wk01hw02/JsonPlaceHolderAPI.java
@@ -0,0 +1,13 @@
+package com.example.wk01hw02;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface JsonPlaceHolderAPI {
+    //this is a retrofit call, this is essentially the endpoint we need to access to get data from the API
+    //Since this will pull all posts, we need to narrow it later to just posts with our userId
+    @GET("posts")
+    Call<List<Post>> getPosts();
+}
---
+++ app/src/main/java/com/example/wk01hw02/Post.java
@@ -0,0 +1,29 @@
+package com.example.wk01hw02;
+
+public class Post {
+    //JSON returned from request has these 4 fields
+    /*NOTE: you could vary these names, but you need to use
+    @SerializedName("jsonvar") to make sure it sees it correctly
+    */
+    private int userId; //id of the user
+    private int id; //id of the post
+    private String title; //title of the post
+    private String body; //body of the post
+
+    //Getters
+    public int getUserId() {
+        return userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+}
